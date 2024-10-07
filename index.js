8f30757a22201cf846ab318bb39c5671d54d485d
Milena <mgaier234@gmail.com>
2024-10-07 11:57:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let menuInfos = prompt("1. Cadastrar abrigo: \n 2. Listar abrigos: \n 3. Procurar abrigo: \n 4. Sair")
+function listaAbrigos(array) {
+    let menu 
+    switch (menu) {
+    case  '1' :
+        prompt()
+
+    }
+    
+}
+
+//como nome, endereço, cidade, telefone e capacidade de lotação.
+
+/*let cadastro = []
+    let infos = {
+        nome :
+        endereço :
+        cidade :
+        telefone : 
+        capacidadeAbrigo : 
+    }*/
