794f5a7ad3d5ba57cde748e279217e7a787c8d7d
Milena <mgaier234@gmail.com>
2024-10-10 18:39:28 -0300

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let menuopcoes = prompt("1. Cadastrar abrigo: \n 2. Listar abrigos: \n 3. Procurar abrigo: \n 4. Sair")
+let menuopcoes = prompt("1. Cadastrar abrigo: \n 2. Listar abrigos: \n 3. Procurar abrigo por cidade: \n 4. Sair")
 let abrigos = []
 
 function cadastro () { 
@@ -6,7 +6,8 @@ function cadastro () {
     endereco = prompt("Endereço do abrigo:")  
     cidade = prompt("Digite a cidade do abrigo:")
     telefone = Number(prompt("Telefone do abrigo:")) 
-    cap = Number (prompt("Capacidade de lotação deste abrigo:"))
+    cap = Number(prompt("Capacidade de lotação deste abrigo:"))
+
 const abrigo = {
 abrigoS : nome ,
 enderecos : endereco ,
@@ -16,31 +17,32 @@ lotacao : cap
 }
 abrigos.push(abrigo)
 }
-
  
-/*function listagem () {
-
-}*/
+function listagem () {
+for (let lista of abrigos) {
+alert(lista)
+}
+}
 
 function procura () {
-prompt("insira nome da cidade")
-alert(cidade)
+cidades = prompt("Qual é a sua cidade?:")
+for (let procurar of abrigos) {
+alert(procurar)
+}
 }
 
 let menu
 switch (menuopcoes) {
-    case '1':
+    case '1': 
         cadastro()
         break
     case '2':
-        alert(abrigos)
+       listagem()
         break
     case '3':
+       procura()
         break
     case '4':
-        Saiu
-        break
-}
         Saiu
         break
 }
